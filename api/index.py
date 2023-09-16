fa8760ee57ecb7e769d8f813f74a037ed9daa3a7
lumartch <lumartch@gmail.com>
2023-09-16 16:20:02 +0200

    Creating src folder to admin correctly files.

--- api/index.py
+++ api/index.py
@@ -1,11 +1,19 @@
 from flask import Flask
+import requests
+
+from src.util.Enums import *
 
 app = Flask(__name__)
+BASE_API_URI = EApiPaths.BASE_API_URI.value + EApiPaths.USER.value + EApiPaths.PATH_PARAM_USER.value
 
-@app.route("/api/user/<username>/info")
+@app.route(BASE_API_URI + EApiPaths.INFO.value)
 def getUser(username):
-    return { "usermane": username }
+    uri = EGitHub.GITHUB_BASE_URI.value + EGitHub.USER.value.replace('{username}', username)
+    r = requests.get(url = uri)
+    return r.json()
 
-@app.route("/api/user/<username>/repos")
+@app.route(BASE_API_URI + EApiPaths.REPOS.value)
 def getRepos(username):
-    return { "usermane": username, "repos": "repositorios" }
+    uri = EGitHub.GITHUB_BASE_URI.value + EGitHub.REPOS.value.replace('{username}', username)
+    r = requests.get(url = uri)
+    return r.json()
