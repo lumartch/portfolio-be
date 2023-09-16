799800d715162df4a9fa2a0f420bb50771522e6f
lumartch <lumartch@gmail.com>
2023-09-16 16:07:27 +0200

    Removing files

--- api/index.py
+++ api/index.py
@@ -1,19 +1,11 @@
 from flask import Flask
-import requests
-
-from util.Enums import *
 
 app = Flask(__name__)
-BASE_API_URI = EApiPaths.BASE_API_URI.value + EApiPaths.USER.value + EApiPaths.PATH_PARAM_USER.value
 
-@app.route(BASE_API_URI + EApiPaths.INFO.value)
+@app.route("/api/user/<username>/info")
 def getUser(username):
-    # uri = EGitHub.GITHUB_BASE_URI.value + EGitHub.USER.value.replace('{username}', username)
-    # r = requests.get(url = uri)
     return { "usermane": username }
 
-@app.route(BASE_API_URI + EApiPaths.REPOS.value)
+@app.route("/api/user/<username>/repos")
 def getRepos(username):
-    # uri = EGitHub.GITHUB_BASE_URI.value + EGitHub.REPOS.value.replace('{username}', username)
-    # r = requests.get(url = uri)
-    return { "usermane": username }
+    return { "usermane": username, "repos": "repositorios" }
