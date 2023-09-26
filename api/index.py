d368f0b916b3ad27ec54e478fff56a7ad5b7eae9
lumartch <lumartch@gmail.com>
2023-09-26 12:48:02 +0200

    Added flask cache

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 import json
-from flask import Flask, request
+from flask import Flask
 from flask_cors import CORS
+from flask_caching import Cache
 from werkzeug.exceptions import HTTPException
 from flask_parameter_validation import ValidateParameters, Route, Query
 
@@ -8,13 +9,17 @@ from src.util.Enums import *
 from src.service.GitHubService import GitHubService
 from src.service.GitLabService import GitLabService
 
+cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 app = Flask(__name__)
 CORS(app)
+cache.init_app(app)
+
 BASE_API_URI = EApiPaths.BASE_API_URI.value + EApiPaths.USER.value + EApiPaths.PATH_PARAM_USER.value
 github_service = GitHubService()
 gitlab_service = GitLabService()
 
 @app.get(BASE_API_URI + EApiPaths.INFO.value)
+@cache.cached(timeout=120)
 @ValidateParameters()
 def getUser(username: str = Route(), git_source: str = Query(EGitSource.ALL.value)):
     if(git_source == EGitSource.GIT_HUB.value):
@@ -31,6 +36,7 @@ def getUser(username: str = Route(), git_source: str = Query(EGitSource.ALL.valu
         return profile
 
 @app.get(BASE_API_URI + EApiPaths.REPOS.value)
+@cache.cached(timeout=120)
 @ValidateParameters()
 def getRepos(username: str = Route(), archived: bool = Query(False), git_source: str = Query(EGitSource.ALL.value)):
     if(git_source == EGitSource.GIT_HUB.value):
